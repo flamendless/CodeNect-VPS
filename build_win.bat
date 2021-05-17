75f535d377fae521bb2601ab336800de71695e22
flamendless <flamendless8@gmail.com>
2021-05-17 22:07:06 +0800

    TCC IS WORKING!

--- build_win.bat
+++ build_win.bat
@@ -4,3 +4,6 @@ set dir_install=bin_win
 
 cmake -G "MinGW Makefiles" -B%dir_build% -H. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%dir_install% -DCMAKE_C_COMPILER=clang.exe -DCMAKE_CXX_COMPILER=clang++.exe
 cmake --build %dir_build% --target install
+cd %dir_install%
+%app_name%.exe
+cd ../
